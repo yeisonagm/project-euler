507b7d2de715c0f61493fae0e3f79d4414f55664
Yeison Garcia <garciamonsefu@gmail.com>
2023-10-25 05:05:40 -0500

    Solution to the first problem

--- src/problems/p001_multiples_of_3_or_5.rs
+++ src/problems/p001_multiples_of_3_or_5.rs
@@ -12,6 +12,40 @@
 /// The sum of all the multiples of 3 or 5 below the specified limit.
 
 
+fn multiples_3or5(last: u32) -> Vec<u32> {
+    (3..last).filter(|&e| e % 3 == 0 || e % 5 == 0).collect()
+}
+
+
 pub fn solve() { 
-    println!("Problem 001 => Multiples of 3 or 5");
+    print!("The sum of multiples below than 10 is ");
+    let multiples_10 = multiples_3or5(10);
+    println!("{}", multiples_10.into_iter().sum::<u32>());
+
+    print!("The sum of multiples below than 1000 is ");
+    let multiples_1000 = multiples_3or5(1000);
+    println!("{}", multiples_1000.into_iter().fold(0, |a, b| a + b));
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiples_3or5() {
+        assert_eq!(multiples_3or5(10), vec![3, 5, 6, 9]);
+        assert_eq!(multiples_3or5(20), vec![3, 5, 6, 9, 10, 12, 15, 18]);
+        assert_eq!(multiples_3or5(0), vec![]);
+        assert_eq!(multiples_3or5(2), vec![]);
+    }
+
+    #[test]
+    fn test_sum_multiples_3or5() {
+        assert_eq!(multiples_3or5(10).into_iter().sum::<u32>(), 23);
+        assert_eq!(multiples_3or5(20).into_iter().sum::<u32>(), 78);
+        assert_eq!(multiples_3or5(0).into_iter().sum::<u32>(), 0);
+        assert_eq!(multiples_3or5(2).into_iter().sum::<u32>(), 0);
+    }
 }
